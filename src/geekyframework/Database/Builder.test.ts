4dd14f0c71769871fd541b0c7916724bcb7a33b9
Ankur Kedia <theankurkedia@gmail.com>
2019-05-02 21:43:53 +0530

    Reset builder test file

--- src/geekyframework/Database/Builder.test.ts
+++ src/geekyframework/Database/Builder.test.ts
@@ -1,37 +1,22 @@
 import Builder from "./Builder";
 import FakeConnection from "./Connection/FakeConnection";
-import FirestoreConnection from "./Connection/Firestore";
 
-// let connection = new FakeConnection();
-let firebaseConnection = new FirestoreConnection();
-let builder = new Builder(firebaseConnection);
+let connection = new FakeConnection();
+let builder = new Builder(connection);
+
+test("Builder: Test where() and query()", async () => {
+  let fetchedValues: any = await builder
+    .select("*")
+    .from("user")
+    .where("name", "=", "Sanket")
+    .query();
+
+  expect(fetchedValues[0].hasOwnProperty("id")).toBe(true);
+});
 
 test("Builder: Test insert()", async () => {
-  let id: any = await builder.from("user").insert({
-    name: "Suraj"
+  let insertedValues: any = await builder.from("user").insert({
+    name: "Sanket"
   });
-  expect(id).toBeDefined();
+  expect(insertedValues.hasOwnProperty("id")).toBe(true);
 });
-// test("Builder: Test where() and query()", async () => {
-//   let fetchedValues: any = await builder
-//     .select("*")
-//     .from("user")
-//     .where("name", "=", "Suraj")
-//     .query();
-
-//   expect(fetchedValues[0]["name"]).toBe("Suraj");
-// });
-// test("Builder: Test delete()", async () => {
-//   let id: any = await builder
-//     .from("user")
-//     .where("name", "=", "Suraj")
-//     .delete();
-//   expect(true).toBe(true);
-// });
-// test("Builder: Test update()", async () => {
-//   let id: any = await builder
-//     .from("user")
-//     .where("name", "=", "Suraj")
-//     .update({name: "Suresh"});
-//   expect(true).toBe(true);
-// });
